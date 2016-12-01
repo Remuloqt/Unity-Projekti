c670b3c7dce90783c48a00061e887b4a99718f0f
Maustemies <saku.rautio@luukku.com>
2016-12-01 21:12:40 +0200

    modified the gitignore file and added functionality to the starting screen

---
+++ Assets/Scripts/StartingScreenScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartingScreenScript : MonoBehaviour {
+
+    public String sceneName;
+    public Text playerName;
+    public Text serverIP;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonQuitClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void OnButtonStartClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
