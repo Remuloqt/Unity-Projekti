c6d87217e4f0955156c525b3b94547c145ce75b6
SakuRautio <t4rasa00@students.oamk.fi>
2016-12-07 11:01:17 +0200

    Added a car controller script for car handling.

---
+++ Assets/Scripts/CarPlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarPlayerController : NetworkBehaviour
+    {
+
+        public Camera camera;
+
+        private CarController m_Car; // the car controller we want to use
+
+        // Use this for initialization
+        void Start()
+        {
+            m_Car = GetComponent<CarController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                camera.enabled = false;
+                return;
+            }
+            // pass the input to the car!
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            m_Car.Move(h, v, v, handbrake);
+#else
+            m_Car.Move(h, v, v, 0f);
+#endif
+        }
+    }
+}
