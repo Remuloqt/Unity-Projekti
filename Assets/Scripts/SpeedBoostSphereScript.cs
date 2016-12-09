65c9ee4819e00b1009a0a11b06e0ead250a4e495
SakuRautio <t4rasa00@students.oamk.fi>
2016-12-09 12:55:03 +0200

    asd

---
+++ Assets/Scripts/SpeedBoostSphereScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoostSphereScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        GameObject collidersObject = collider.transform.parent.gameObject;
+        GameObject carObject = collidersObject.transform.parent.gameObject;
+
+        Vector3 tempVelocity = carObject.GetComponent<Rigidbody>().velocity;
+        tempVelocity.z *= 1.7f;
+        carObject.GetComponent<Rigidbody>().velocity = tempVelocity;
+    }
+}
