60640711fa3991ebf29d0f389d0b470cc408331c
SakuRautio <t4rasa00@students.oamk.fi>
2016-12-15 13:13:07 +0200

    Made the terrain collision detection better at the cost of lowering the underlying terrain.

--- Assets/Scripts/ColliderOutOfBoundsScript.cs
+++ Assets/Scripts/ColliderOutOfBoundsScript.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelOutOfBoundsScript : MonoBehaviour {
+public class ColliderOutOfBoundsScript : MonoBehaviour {
 
     private GameObject playerObject;
 
 	// Use this for initialization
 	void Start () {
+        /*
         GameObject wheelsHub = transform.parent.gameObject;
         playerObject = wheelsHub.transform.parent.gameObject;
+        */
+        GameObject collidersObject = transform.parent.gameObject;
+        playerObject = collidersObject.transform.parent.gameObject;
 	}
 	
+    /*
 	void FixedUpdate () {
         WheelHit hit;
         WheelCollider wheel = GetComponent<WheelCollider>();
@@ -24,5 +29,14 @@ public class WheelOutOfBoundsScript : MonoBehaviour {
             }
         }
 	}
+    */
 
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.name == "Terrain")
+        {
+            CarOutOfBoundsScript carOutOfBoundsScript = playerObject.GetComponent<CarOutOfBoundsScript>();
+            carOutOfBoundsScript.OnPlayerGroundHit();
+        }
+    }
 }
